18651e5bea79f8b8c9da465f78485b12820af722
Jonas Muylaert <30555565+JonasMuylaert@users.noreply.github.com>
2020-10-29 11:45:52 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var cors = require('cors');
 app.use(
     cors({
         credentials: true,
-        origin: 'https://testingwebsecurity.herokuapp.com/'
+        origin: '*'
     })
 );
 app.options('*', cors());
